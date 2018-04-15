01ef8958f271e4e4b51f2ca202617992fc7c753c
debarron <de.lopezbarron@gmail.com>
2018-04-15 03:08:55 -0500

    Testing local processing in tile extraction
    Former-commit-id: c35565c3e2a0844e675d7ba9f30ed41578b074b3

--- src/main/scala/nidan/test/SparkTileGenerator2.scala
+++ src/main/scala/nidan/test/SparkTileGenerator2.scala
@@ -171,8 +171,8 @@ object SparkTileGenerator2 {
     
     // Write the data in local
     val (rddWriteTiles, timeW) = NidanUtils.timeIt{ 
-      rddTiles1.mapPartitions(partitionGroups1, true)
-//      rddTiles1.foreachPartition(partitionGroups1)
+//      rddTiles1.mapPartitions(partitionGroups1, true)
+      rddTiles1.foreachPartition(partitionGroups1)
     }
     
     
@@ -185,16 +185,16 @@ object SparkTileGenerator2 {
 //    
     
 //    val originalTotal = rddTiles1.count
-    val writes = rddWriteTiles.map(el => el._2).sum
-    val errors = rddWriteTiles.map(el => el._1).sum
+//    val writes = rddWriteTiles.map(el => el._2).sum
+//    val errors = rddWriteTiles.map(el => el._1).sum
     
 //    val reads
 //    val total = data.count
 //    val error = data.filter(_._1 == 1).count
 //    val success = data.filter(_._1 == 0).count
-    logger.info(s">> Original Total: ${totalEls}")
-    logger.info(s">> Total writes: ${writes}")
-    logger.info(s">> Errors: ${errors}")
+//    logger.info(s">> Original Total: ${totalEls}")
+//    logger.info(s">> Total writes: ${writes}")
+//    logger.info(s">> Errors: ${errors}")
 //    logger.info(s">> Success: ${success}")
 //    
 //    // 2. Change to Dataframe 
@@ -268,8 +268,6 @@ object SparkTileGenerator2 {
   }
   
   def partitionGroups1(it:Iterator[(String, String, TileMetadata)]):Iterator[(Int,Int)] = {
-    if(it.isEmpty) return Seq((1, 0)).toIterator
-    
     val file = it.toSeq.head._1
     val localOutput = it.toSeq.head._2
     val os = new OpenSlide(new File(file))
